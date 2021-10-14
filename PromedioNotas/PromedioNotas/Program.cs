0d3e4a4ac057ff2c33170eaac5b475ae6d23590d
AndyCruz81 <80351895+AndyCruz81@users.noreply.github.com>
2021-10-14 10:18:58 -0600

    Add files via upload

---
+++ PromedioNotas/PromedioNotas/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PromedioNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombreEstudiante;
+            int clases;
+
+            Console.Write("Digite su nombre: ");
+            nombreEstudiante = Console.ReadLine();
+
+            Console.WriteLine($"{Environment.NewLine}{nombreEstudiante} ¿Cuántas clases llevas?");
+            clases = Convert.ToInt32(Console.ReadLine());
+
+            float[] notas = new float[clases];
+            float suma = 0;
+            float mayor = 0;
+
+            for (int i=0; i< notas.Length; i++)
+            {
+                Console.Write($"Digite la nota para la clase {i + 1}: ");
+
+                notas[i] = (float)Convert.ToDouble(Console.ReadLine());
+
+                suma += notas[i];
+
+                if (i == 0)
+                {
+                    mayor = notas[i];
+                }
+                else
+                {
+                    if (notas[i] > mayor)
+                    {
+                        mayor = notas[i];
+                    }
+                }
+            }
+
+            float promedio = suma / notas.Length;
+
+            Console.WriteLine(Environment.NewLine + "Su promedio de notas es : " + promedio);
+            Console.WriteLine($"Su nota más alta es {mayor}");
+
+        }
+    }
+}
